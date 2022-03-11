fd5a11cd63d7d8760b0f27eab60477640fa0d7b6
SuyHeng <101233511+SuyHeng@users.noreply.github.com>
2022-03-11 09:45:40 +0700

    Add files via upload

---
+++ AgeNameCS.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    //variables
+    int age;
+    string name;
+
+    //output display for user input
+    cout << "Age : ";
+    cin >> age;
+    cout << "Name : ";
+    cin >> name;
+    
+    //calculate input
+    if (age % 2 == 0)       //calculate for remainder with modulus(%), if remainder = 0, then it is even. Otherwise it is odd.
+    {
+        cout << name << endl;
+        cout << name << endl;
+        cout << name << endl;
+        cout << name << endl;
+        cout << name << endl;
+    }
+    else 
+    {
+        cout << name << endl;
+        cout << name << endl;
+        cout << name << endl;
+        cout << name << endl;
+        cout << name << endl;
+        cout << name << endl;
+        cout << name << endl;
+        cout << name << endl;
+        cout << name << endl;
+        cout << name << endl;
+
+    }
+
+    return 0;
+}
