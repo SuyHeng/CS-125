bb1c865e53fdc6673799b7f9a58e0becea33a5b7
Suy Heng <101233511+SuyHeng@users.noreply.github.com>
2022-04-01 04:42:09 +0700

    Update primenumber.cpp

--- primenumber.cpp
+++ primenumber.cpp
@@ -3,19 +3,19 @@ using namespace std;
 int main()
 
 {
-    int num;
+    int num, i, f;
     bool isPrime = true;
-
+    
     cout << "Enter your number: ";
     cin >> num;
 
-    //Since 0 and 1 are not prime numbers
+    //check if number is 0 or 1, since 0 or 1 is neither composite nor prime
     if (num == 0 || num == 1)
     {
         isPrime = false;
     }
 
-    //Calculate for prime numbers
+    //check if the number is a prime number
     for (int i = 2; i <= num / 2; i++)
     {
         if (num % 2 == 0)
@@ -28,7 +28,13 @@ int main()
     if (isPrime)
     cout << num << " is a Prime number." << endl;
     else
-    cout << num << " is not a Prime number." << endl;
-
+    cout << num << " is a Composite number." << endl;
+    cout << "Factors of " << num << " are: " ;
+    //check for factors if number is a composite number
+    for (f = 1; f <= num; f++)
+    {
+        if (num % f == 0)
+        cout << f << " ";
+    }
     return 0;
 }
